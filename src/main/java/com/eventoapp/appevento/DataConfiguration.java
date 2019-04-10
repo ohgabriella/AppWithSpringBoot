40a2a42112ad7028ea98c7232060580487e51243
unknown <gabriellabarretogomes@gmail.com>
2019-04-10 11:04:54 -0300

    Configurando o ambiente de produção e desenvolvimento

--- src/main/java/com/eventoapp/appevento/DataConfiguration.java
+++ src/main/java/com/eventoapp/appevento/DataConfiguration.java
@@ -8,12 +8,14 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
 	@Bean
 	public DataSource dataSource() {
